d7e22492f19fd2756416aabeef711ce8686eb232
Roman Janusz <roman@student.agh.edu.pl>
2012-12-05 21:13:28 +0100

    modified jage

---
+++ jage/algorithms/solutions/genetic/src/main/java/org/jage/genetic/StatsCollector.java
@@ -0,0 +1,11 @@
+package org.jage.genetic;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ghik
+ * Date: 04.12.12
+ * Time: 23:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class StatsCollector {
+}
